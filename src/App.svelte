d220f64b5225b820c71ba13bda46597eb2f67445
Lawrence N. Masullo <lmasullo@gmail.com>
2020-12-01 08:39:24 -0600

    Initial

---
+++ src/App.svelte
@@ -0,0 +1,31 @@
+<script>
+  // export let name;
+</script>
+
+<style>
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
+
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
+
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
+</style>
+
+<main>
+  <h1>EM Resus is under construction.</h1>
+  <h3>Check back later.</h3>
+  <!-- <p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p> -->
+</main>
