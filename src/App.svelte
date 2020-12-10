e7b19f5e0f037090e4e2b533418f2d68faaad06a
Lawrence N. Masullo <lmasullo@gmail.com>
2020-12-10 10:24:04 -0600

    Made it responsive

--- src/App.svelte
+++ src/App.svelte
@@ -6,7 +6,7 @@
   main {
     text-align: center;
     padding: 1em;
-    max-width: 240px;
+    /* max-width: 240px; */
     margin: 0 auto;
   }
 
@@ -17,15 +17,17 @@
     font-weight: 100;
   }
 
-  @media (min-width: 640px) {
-    main {
-      max-width: none;
+  @media (max-width: 425px) {
+    .container {
+      font-size: 0.5rem;
     }
   }
 </style>
 
 <main>
-  <h1>EM Resus is under construction.</h1>
-  <h3>Check back later.</h3>
-  <!-- <p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p> -->
+  <div class="container">
+    <h1>EM Resus is under construction.</h1>
+    <h3>Check back later.</h3>
+    <!-- <p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p> -->
+  </div>
 </main>
